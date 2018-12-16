f11797ecf54dbcef3bd743428e6681b7700f7e08
Sam Gosling <sxg@Sams-MacBook-Pro.local>
2018-12-16 15:34:27 +0100

    basic dApp ABI initialisation + deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,68 +1,59 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./utils/getWeb3";
+import Button from "@atlaskit/button";
+
+import Validation from "./contracts/communalValidation.json";
+import ERC20d from "./contracts/ERC20d.json";
+
 import truffleContract from "truffle-contract";
+import getWeb3 from "./utils/getWeb3";
 
 import "./App.css";
 
+const decimal = Math.pow(10,18);
+
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { web3: null, account: null, token: null , dapp: null };
+
+  componentDidUpdate = async () => {
+      await this.getBalances();
+  }
 
   componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
+      const web = await getWeb3()
+      const accounts = await web.eth.getAccounts()
 
-      // Get the contract instance.
-      const Contract = truffleContract(SimpleStorageContract);
-      Contract.setProvider(web3.currentProvider);
-      const instance = await Contract.deployed();
+      const AB1 = truffleContract(ERC20d)
+      const AB2 = truffleContract(Validation)
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`
-      );
-      console.log(error);
-    }
+      AB1.setProvider(web.currentProvider)
+      AB2.setProvider(web.currentProvider)
+
+      const instance1 = await AB1.deployed()
+      const instance2 = await AB2.deployed()
+
+      await this.setState({
+        account: accounts[0],
+          token: instance1,
+           dapp: instance2,
+           web3: web })
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.set(5, { from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.get();
-
-    // Update state with the result.
-    this.setState({ storageValue: response.toNumber() });
-  };
+  getBalances = async() => {
+    const EGEM = (parseFloat(await this.state.web3.eth.getBalance(this.state.account))/decimal).toFixed(2);
+    const VLDY = (parseFloat(await this.state.token.balanceOf(this.state.account))/decimal).toFixed(2)
+    await this.setState({
+      tokenBal: VLDY,
+      gasBal: EGEM })
+  }
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>VLDY MVP</h1>
+        <h2> {this.state.account} </h2>
+        <h3> {this.state.gasBal} &nbsp;EGEM</h3>
+        <h3> {this.state.tokenBal} &nbsp;VLDY</h3>
       </div>
     );
   }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,13 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Validation = artifacts.require("./communalValidation.sol");
+var AddressSet = artifacts.require("./addressSet.sol");
+var SafeMath = artifacts.require("./SafeMath.sol");
+var ERC20d = artifacts.require("./ERC20d.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+  deployer.deploy(SafeMath);
+  deployer.deploy(AddressSet);
+  deployer.link(SafeMath, ERC20d);
+  deployer.deploy(ERC20d);
+  deployer.link(AddressSet, Validation);
+  deployer.deploy(Validation);
 };
