b6e956c200bb37cd5d77e04514a6d2cfe819f25d
Sam Gosling <samuel.jj.gosling@gmail.com>
2019-03-06 00:47:29 +0100

    tidyup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component, Fragment } from 'react';
 
+import {
+  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip,
+} from 'recharts';
+
 import { Segment, Icon, Table } from 'semantic-ui-react'
 import TextField from '@atlaskit/field-text';
 import Toggle from 'react-toggle'
@@ -53,6 +57,30 @@ const ethIcon = () => <FontAwesomeIcon color="#0cff6f" icon={faEthereum} classNa
 const bullseyeIcon = () => <FontAwesomeIcon color="#0cff6f" icon={faBullseye} className="starIcon" size='1x'/>
 
 const themeModes = { light, dark, settings };
+
+const data = [
+  {
+    name: '0', uv: 4000, pv: 2400, amt: 2400,
+  },
+  {
+    name: '1', uv: 3000, pv: 1398, amt: 2210,
+  },
+  {
+    name: '2', uv: 2000, pv: 9800, amt: 2290,
+  },
+  {
+    name: '3', uv: 2780, pv: 3908, amt: 2000,
+  },
+  {
+    name: '4', uv: 1890, pv: 4800, amt: 2181,
+  },
+  {
+    name: '5', uv: 2390, pv: 3800, amt: 2500,
+  },
+  {
+    name: '6', uv: 3490, pv: 4300, amt: 2100,
+  },
+]
 
 const GlobalNavigation = () => (
   <GlobalNav primaryItems={[
@@ -540,6 +568,25 @@ class App extends Component {
         <div className="eventBorder">
         </div>
 
+        <div className="eventGraph">
+        <AreaChart
+        width={587.5}
+        height={287.5}
+        data={data}
+        margin={{
+          top: 10, right: 30, left: 0, bottom: 0,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Area type="monotone" dataKey="uv" stackId="1" stroke="#ff0c9e" fill="#ff0c9e" />
+        <Area type="monotone" dataKey="pv" stackId="1" stroke="cyan" fill="cyan" />
+        <Area type="monotone" dataKey="amt" stackId="1" stroke="#0cff6d" fill="#0cff6d" />
+        </AreaChart>
+        </div>
+
         <div className="statModal">
 
         </div>
@@ -612,7 +659,7 @@ class App extends Component {
              <th textAlign="center" colSpan='1'>
             <div className="textColor">
             <FontAwesomeIcon color="#0cff6f" icon={faStar} size='lg'/>
-             &nbsp;&nbsp;Type
+             &nbsp;&nbsp;Validator
              </div>
              </th>
              <th textAlign="center" colSpan='1'>
